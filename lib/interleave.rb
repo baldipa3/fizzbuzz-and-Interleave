d8b4b98414c79ed5d2d10c8bc533ed5d06801b63
Pablo Baldini <pablo.baldini@gmail.com>
2021-06-03 10:42:04 -0300

    Interleave refactoring

--- lib/interleave.rb
+++ lib/interleave.rb
@@ -4,27 +4,18 @@ def interleave(a, b)
 
   raise ArgumentError.new("Array cant be the same size") if a_size == b_size
 
-  return a if b.empty?
-  return b if a.empty?
+  biggest_array = [a,b].max_by(&:size).dup
+  smallest_array = [a,b].min_by(&:size)
 
-  interloop = 0
+  return a if smallest_array.empty?
+
+  interloop = biggest_array.size / smallest_array.size
   sum = 0
 
-  if a_size > b_size
-    interloop = a_size / b_size
-    b.each do |element|
-      sum += interloop
-      a.insert(sum, element)
-      sum += 1
-    end
-    return a
-  else
-    interloop = b_size / a_size
-    a.each do |element|
-      sum += interloop
-      b.insert(sum, element)
-      sum += 1
-    end
-    return b
+  smallest_array.each do |element|
+    sum += interloop
+    biggest_array.insert(sum, element)
+    sum += 1
   end
+  return biggest_array
 end
